fff0236a0c495320898cee589d1f4f989367a03e
jinnkim0112 <jinnkim0112@gmail.com>
2023-06-19 23:13:54 +0900

    part1 finished

--- firstproject/src/App.js
+++ firstproject/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   let postName = '강남 우동 맛집';              // 이 부분은 바뀌어도 html 자동변경 안됨
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '라면 추천']); // 이 부분은 바뀌면 html 자동 재렌더링
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '라면 추천', '파이썬 강의']); // 이 부분은 바뀌면 html 자동 재렌더링
   let [따봉, 따봉변경] = useState(0);
-  
+
+
+  let [따봉들, 따봉들변경] = useState([...글제목].map(function(){return 0;}));
+  let [modal, setModal] = useState(-1);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
@@ -13,7 +17,7 @@ function App() {
         <h4>블로그임</h4>
       </div>
 
-      <button onClick = {()=>{ // function 자동으로 만드는 함수
+      {/* <button onClick = {()=>{ // function 자동으로 만드는 함수
         let copy = [...글제목];
         copy.sort();
         글제목변경(copy);       // 얘를 통해 글제목 변경 가능
@@ -32,25 +36,83 @@ function App() {
       <div className='list'>
         <h4>{ 글제목[0] }<span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span>{ 따봉 }</h4>
         <p>날짜</p>
-      </div>
+      </div> */}
 
-      <div className='list'>
-        <h4>{ 글제목[1] }</h4>
-        <p>날짜</p>
-      </div>
+      
 
-      <Modal/>
+      {
+        글제목.map(function(x, i){
+          return (
+          <div className='list' key={i}>
+            <h4>
+              <span onClick={()=>{setModal(modal === i ? -1 : i)}}>{ x }</span>
+              <span onClick={()=>{
+                let copy = [...따봉들];
+                copy[i] = 따봉들[i]+1;
+              따봉들변경(copy)
+              }}>
+                👍
+              </span>
+              { 따봉들[i] }
+              </h4>
+            <p>날짜</p>
+            <button onClick={() => {
+              let copy = [...글제목];
+              copy.splice(i, 1);
+              글제목변경(copy)
+            }}>del</button>
+          </div>
+        )
+        })
+      }
 
+
+      {
+        modal !== -1 ? <Modal 글제목={글제목} modal={modal} 글제목변경={글제목변경}/> : null
+      }
+
+      <input onChange={(e)=>{
+        입력값변경(e.target.value)
+      }}/>
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy.unshift(입력값);
+        글제목변경(copy)
+      }}>만들기</button>
+
+      <Modal2></Modal2>
     </div>
   );
 }
 
-function Modal(){ // 대문자여야함
+class Modal2 extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name : 'kim',
+      age : 20
+    };
+  }
+  render(){
+    return(
+      <div> 안녕 {this.state.age}
+        <button onClick={()=>{
+          this.setState({age : 21})
+        }}>버튼</button>
+      </div>
+    )
+  }
+}
+
+function Modal(props){ // 대문자여야함
   return(
     <div className='model'>
-      <h4>제목</h4>
+      <h4>{props.글제목[props.modal]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={()=>{let copy = [...props.글제목];
+        copy[0] = '여자 코트 추천';
+        props.글제목변경(copy)}}>글수정</button>
     </div>
   )
 }
